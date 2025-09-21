79fd0de0ddf3f4bb551ed50aad7aa170d4bd513d
Tejasvi-Chauhan <chauhantejasvi07@gmail.com>
2025-09-22 00:20:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-if(process.env.NODE_ENV!=="production"){
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
 const express = require("express");
 const app = express();
 const path = require("path");
 const ejsMate = require("ejs-mate");
-const { listingschema  , reviewSchema} = require("./views/schema.js");
+const { listingschema, reviewSchema } = require("./views/schema.js");
 const methodOverride = require("method-override");
 const mongoose = require("mongoose");
-const listings=require("./routes/listing.js");
-const reviewroutes=require("./routes/review.js");
+const listings = require("./routes/listing.js");
+const reviewroutes = require("./routes/review.js");
 const userRoutes = require("./routes/user.js");
-const session =require('express-session');
-const MongoStore = require('connect-mongo');
-const flash = require('connect-flash');
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const flash = require("connect-flash");
 const passport = require("passport");
 const localStrategy = require("passport-local");
 const User = require("./models/user.js");
@@ -22,11 +22,10 @@ const { createSecretKey } = require("crypto");
 const port = 8080;
 
 // Connect to MongoDB
-const dbURL=process.env.ATLASDB_URI;
+const dbURL = process.env.ATLASDB_URI;
 async function main() {
   await mongoose.connect(dbURL);
 }
-
 
 main()
   .then(() => {
@@ -36,7 +35,6 @@ main()
     console.log(err);
   });
 
-
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
@@ -44,20 +42,17 @@ app.engine("ejs", ejsMate);
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "/public")));
 
-
-
 const store = MongoStore.create({
   mongoUrl: dbURL,
   touchAfter: 24 * 60 * 60, // time period in seconds
   crypto: {
     secret: process.env.SECRET,
-  }
+  },
 });
- 
+
 store.on("error", function (e) {
   console.log("ERROR in MONGO SESSION STORE", e);
 });
-
 
 const sessionOptions = {
   store: store,
@@ -65,13 +60,11 @@ const sessionOptions = {
   resave: false,
   saveUninitialized: true,
   cookie: {
-    expires:Date.now() +  7 * 24 * 60 * 60 * 1000, // 7 days
-    maxAge: 7 * 24 * 60 * 60 * 1000,  
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000, // 7 days
+    maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true, // Helps prevent XSS attacks
   },
-}
-
-
+};
 
 app.use(session(sessionOptions));
 app.use(flash());
@@ -90,22 +83,30 @@ passport.deserializeUser(User.deserializeUser());
 //   res.send("Working");
 // });
 
-app.use((req,res,next)=>{
-  res.locals.success=req.flash("success");
-   res.locals.error = req.flash("error");
-   res.locals.currentUser = req.user; // Make the current user available in all views
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currentUser = req.user; // Make the current user available in all views
   next();
-})
+});
 
+app.use("/listings", listings);
+app.use("/listings/:id/reviews", reviewroutes);
+app.use("/", userRoutes);
 
-app.use("/listings",listings);
-app.use("/listings/:id/reviews",reviewroutes);
-app.use("/",userRoutes);
+// Redirect homepage to listings
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
+
+// Catch-all for undefined routes
+app.all("*", (req, res) => {
+  res.redirect("/listings");
+});
 
 app.listen(port, () => {
   console.log(`App is listening at ${port}`);
 });
-
 
 app.use((err, req, res, next) => {
   let { status = 401, message = "Something went wrong" } = err;
